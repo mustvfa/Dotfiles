0921d78de6bf3e31b5dc55d87aff87be7bf1c41c
mustvfa <mustvfa@proton.me>
2026-01-21 21:45:34 +0000

    so sigma

--- InstallScripts/waybarinstall.sh
+++ InstallScripts/waybarinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yay -S waybar wl-color-picker grim slurp wl-clipboard otf-codenewroman-nerd pywal
-wal -i ~/Dotfiles/wallpapers/pywallpaper.jpg
+wal -i ~/Dotfiles/wallpapers/walls/MistyTrees.jpg
 yay -S blueman bluez 
 systemctl enable bluetooth
 sudo cp -a ~/Dotfiles/.config/waybar ~/.config/
